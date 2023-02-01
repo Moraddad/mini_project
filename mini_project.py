510ac16855635828228f9ef9cfe7f221b749b386
Saman <savedoroudi@gmail.com>
2023-02-01 15:53:59 +1100

    adding couriers

--- mini_project.py
+++ mini_project.py
@@ -1,16 +1,26 @@
+product_file = open("products.txt", "r+")
+courier_file = open("couriers.txt", "r+")
+product_list = []
+courier_list = []
+for line in product_file.readlines():
+    product_list.append(line.rstrip())
 
-product_list = ["Pizza", "Pasta", "Cheese Burger", "Coke", "Fanta", "Salad"]
+for line in courier_file.readlines():
+    courier_list.append(line.rstrip())
 
-def product_menu(user_choice):
-    if user_choice == "0":
+
+def product_menu(product_menu_choice):
+    if product_menu_choice == "0":
         main_menu()
-    elif user_choice == "1":
+    elif product_menu_choice == "1":
         print(product_list)
-    elif user_choice == "2":
+    elif product_menu_choice == "2":
         new_prodcut = input("Enter new product's name:\n")
         product_list.append(new_prodcut)
         print(product_list)
-    elif user_choice == "3":
+        product_file.write(new_prodcut + "\n")
+        
+    elif product_menu_choice == "3":
         index = 1
         for product in product_list:
             print(f"[{index}] : {product} ")
@@ -19,7 +29,7 @@ def product_menu(user_choice):
         updated_name = input("Enter the new name: \n")
         product_list[updated_index - 1] = updated_name
         print(product_list)
-    elif user_choice == "4":
+    elif product_menu_choice == "4":
         index = 1
         for product in product_list:
             print(f"[{index}] : {product} ")
@@ -29,11 +39,40 @@ def product_menu(user_choice):
         print(product_list)
 
 
+def courier_menu(courier_menu_choice):
+    if courier_menu_choice == "0":
+        main_menu()
+    elif courier_menu_choice == "1":
+        print(courier_list)
+    elif courier_menu_choice == "2":
+        new_courier = input("Enter new courier's name:\n")
+        courier_list.append(new_courier)
+        print(courier_list)
+        courier_file.write(new_courier + "\n")
+    elif courier_menu_choice == "3":
+        index = 1
+        for courier in courier_list:
+            print(f"[{index}] : {courier} ")
+            index += 1
+        updated_index = int(input("Which courier do you want to update? enter the code:\n" ))
+        updated_name = input("Enter the new name: \n")
+        courier_list[updated_index - 1] = updated_name
+        print(courier_list)
+    elif courier_menu_choice == "4":
+        index = 1
+        for courier in courier_list:
+            print(f"[{index}] : {courier} ")
+            index += 1
+        updated_index = int(input("Which courier do you want to delete? enter the code:\n" ))
+        courier_list.pop(updated_index - 1)
+        print(courier_list)
+        
 
 def main_menu():
     print("WELCOME to Main Menu!")
-    first_choice = input("Press 1 to see the product menu/ Press 0 to exit.\n")
+    first_choice = input("Press 1 to see the product menu/Press 2 to enter the couriers menu/ Press 0 to exit.\n")
     if first_choice == "0":
+        # product_file.close()
         exit()
     elif first_choice == "1":
         second_choice = input(
@@ -45,19 +84,24 @@ def main_menu():
         Press 4 to delete a product.\n
         """
         )
-        return second_choice
+        product_menu(second_choice)
+    elif first_choice == "2":
+        second_choice = input(
+        """
+        Press 0 to return to main menu,
+        Press 1 to see courier list,
+        Press 2 to add a new courier to the list,
+        Press 3 to update an existing courier,
+        Press 4 to delete a courier.\n
+        """
+        )
+        courier_menu(second_choice)
+
+
 
 while True:
-    second_choice = main_menu()
-    product_menu(second_choice)
-    if_continue = input("do you want to continue? Y/N: ")
-    if if_continue == "N"  :
+    main_menu()
+    should_continue = input("do you want to continue? Y/N: ")
+    if should_continue != "Y":
+        product_file.close()
         break
-
-
-print("new line")
-    
-
-print("Sadaf")
-
-
