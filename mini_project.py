b18dc2340e7678fe77fa8e45919fc723366c5cac
Saman <savedoroudi@gmail.com>
2023-02-03 00:06:10 +1100

    Adding couriers and functions

--- mini_project.py
+++ mini_project.py
@@ -7,82 +7,25 @@ for line in product_file:
 for line in courier_file:
     courier_list.append(line.rstrip())
 product_menu_message = """
-        Press 0 to return to main menu,
-        Press 1 to see product list,
-        Press 2 to add a new product to the list,
-        Press 3 to update an existing product,
-        Press 4 to delete a product.
+        Press [0] to RETURN to main menu,
+        Press [1] to PRINT product list,
+        Press [2] to ADD a new product to the list,
+        Press [3] to UPDATE an existing product,
+        Press [4] to DELETE a product.
         """
 courier_menu_message = """
-        Press 0 to return to main menu,
-        Press 1 to see courier list,
-        Press 2 to add a new courier to the list,
-        Press 3 to update an existing courier,
-        Press 4 to delete a courier.
+        Press [0] to RETURN to main menu,
+        Press [1] to PRINT courier list,
+        Press [2] to ADD a new courier to the list,
+        Press [3] to UPDATE an existing courier,
+        Press [4] to DELETE a courier.
         """
 main_menu_message = """
-        Press 1 to see the products menu,
-        Press 2 to enter the couriers menu,
-        Press 0 to exit
+        Press [1] to enter the PRODUCTS MENU,
+        Press [2] to enter the COURIERS MENU,
+        Press [0] to EXIT
         """
 
-def product_menu(product_menu_choice):
-    if product_menu_choice == "0":
-        main_menu()
-    elif product_menu_choice == "1":
-        print(f"Products List: {product_list}")
-    elif product_menu_choice == "2":
-        new_prodcut = input("Enter new product's name:\n")
-        product_list.append(new_prodcut)
-        print(f"New Products List: {product_list}")
-        product_file.write(new_prodcut + "\n")
-    elif product_menu_choice == "3":
-        index = 1
-        for product in product_list:
-            print(f"[{index}] : {product} ")
-            index += 1
-        updated_index = int(input("Which product do you want to update? enter the code:\n" ))
-        updated_name = input("Enter the new name: \n")
-        product_list[updated_index - 1] = updated_name
-        print(f"Updated Products List: {product_list}")
-    elif product_menu_choice == "4":
-        index = 1
-        for product in product_list:
-            print(f"[{index}] : {product} ")
-            index += 1
-        updated_index = int(input("Which product do you want to delete? enter the code:\n" ))
-        product_list.pop(updated_index - 1)
-        print(product_list)
-
-
-def courier_menu(courier_menu_choice):
-    if courier_menu_choice == "0":
-        main_menu()
-    elif courier_menu_choice == "1":
-        print(f"Couriers List: {courier_list}")
-    elif courier_menu_choice == "2":
-        new_courier = input("Enter new courier's name:\n")
-        courier_list.append(new_courier)
-        print(f"New Couriers List: {courier_list}")
-        courier_file.write(new_courier + "\n")
-    elif courier_menu_choice == "3":
-        index = 1
-        for courier in courier_list:
-            print(f"[{index}] : {courier} ")
-            index += 1
-        updated_index = int(input("Which courier do you want to update? enter the code:\n" ))
-        updated_name = input("Enter the new name: \n")
-        courier_list[updated_index - 1] = updated_name
-        print(f"Updated Couriers List: {courier_list}")
-    elif courier_menu_choice == "4":
-        index = 1
-        for courier in courier_list:
-            print(f"[{index}] : {courier} ")
-            index += 1
-        updated_index = int(input("Which courier do you want to delete? enter the code:\n" ))
-        courier_list.pop(updated_index - 1)
-        print(courier_list)
-        
 
 def main_menu():
     print("WELCOME to Main Menu!")
@@ -109,6 +52,70 @@ def main_menu():
         print("Please enter the correct input!")
         main_menu()
 
+
+def product_menu(product_menu_choice):
+    if product_menu_choice == "1":
+        print(f"Products List: {product_list}")
+    elif product_menu_choice == "2":
+        adder(product_file, product_list)
+    elif product_menu_choice == "3":
+        updater(product_file, product_list)
+    elif product_menu_choice == "4":
+        remover(product_file, product_list)
         
+
+def courier_menu(courier_menu_choice):
+    if courier_menu_choice == "1":
+        print(f"Couriers List: {courier_list}")
+    elif courier_menu_choice == "2":
+        adder(courier_file, courier_list)
+    elif courier_menu_choice == "3":
+        updater(courier_file, courier_list)
+    elif courier_menu_choice == "4":
+        remover(courier_file, courier_list)
+    
+ 
+def adder(input_file, input_list):
+    new_item = input("Enter new item's name:\n")
+    input_list.append(new_item)
+    print(f"New List: {input_list}")
+    input_file.write(new_item + "\n")
+
+
+def updater(input_file, input_list):
+    index = 1
+    for item in input_list:
+        print(f"[{index}] : {item} ")
+        index += 1
+    updated_index = int(input("Which item do you want to update? enter the code:\n" ))
+    updated_name = input("Enter the new name: \n")
+    old_name = input_list[updated_index - 1]
+    input_list[updated_index - 1] = updated_name
+    print(f"Updated List: {input_list}")
+    input_file.seek(0)
+    for i in input_list:
+        if i != old_name:
+            input_file.write(i + "\n")
+        elif i == old_name:
+            input_file.write(updated_name + "\n")
+    return input_file.truncate()
+
+
+def remover(input_file, input_list):
+    index = 1
+    for item in input_list:
+        print(f"[{index}] : {item} ")
+        index += 1
+    deleted_index = int(input("Which item do you want to delete? enter the code:\n" ))
+    deleted_name = input_list[deleted_index - 1]
+    input_list.pop(deleted_index - 1)
+    print(f"New List: {input_list}")
+    input_file.seek(0)
+    for i in input_list:
+        if i != deleted_name:
+            input_file.write(i + "\n")
+    return input_file.truncate()
+
+
 while True:
     main_menu()
