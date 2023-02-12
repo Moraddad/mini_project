8d17d37d629d2c98e9ee2244948fdf2c028fb50d
Saman <savedoroudi@gmail.com>
2023-02-12 18:03:49 +1100

    making products and couriers to dictionaries

--- functions.py
+++ functions.py
@@ -1,9 +1,10 @@
 import json
 from messages import *
 
+
 def products_menu(product_menu_choice, p_list):
     if product_menu_choice == "1":
-        print(f"Products List: {p_list}")
+        print_items(p_list)
     elif product_menu_choice == "2":
         add_item(p_list, "product")
     elif product_menu_choice == "3":
@@ -16,7 +17,7 @@ def products_menu(product_menu_choice, p_list):
 
 def couriers_menu(courier_menu_choice, c_list):
     if courier_menu_choice == "1":
-        print(f"Couriers List: {c_list}")
+        print_items(c_list)
     elif courier_menu_choice == "2":
         add_item(c_list, "courier")
     elif courier_menu_choice == "3":
@@ -30,14 +31,13 @@ def couriers_menu(courier_menu_choice, c_list):
 def orders_menu(order_menu_choice, order_list, c_list):
     if order_menu_choice == "1":
         print("Orders List: ")
-        for order in order_list:
-            print(order)
+        print_items(order_list)
     elif order_menu_choice == "2":
         add_order(order_list, c_list)
     elif order_menu_choice == "3":
         change_order_status(order_list)
     elif order_menu_choice == "4":
-        update_order(order_list)
+        update_order(order_list, c_list)
     elif order_menu_choice == "5":
         remove_order(order_list)
     else:
@@ -45,35 +45,39 @@ def orders_menu(order_menu_choice, order_list, c_list):
           
 def add_item(input_list, item_type):
     new_item = input(f"Enter new {item_type}'s name:\n")
-    input_list.append(new_item)
-    print(f"New {item_type}s list: {input_list}")
+    new_number = str(len(input_list) + 1)
+    input_list[new_number] = new_item
+    print(f"New {item_type}s list: ")
+    print_items(input_list)
     with open(f"{item_type}.json", "w") as file:
-        json.dump(input_list, file)
+        json.dump(input_list, file, indent=4)
 
 
 def update_item(input_list,item_type):
-    index = 1
-    for item in input_list:
-        print(f"[{index}] : {item} ")
-        index += 1
-    updated_index = int(input(f"Which {item_type} do you want to update? enter the code:\n" ))
+    print_items(input_list)
+    chosen_number = get_correct_input(input_list, "update")
     updated_name = input(f"Enter the new {item_type}'s name: \n")
-    input_list[updated_index - 1] = updated_name
-    print(f"Updated {item_type}s list: {input_list}")
+    input_list[chosen_number] = updated_name
+    print(f"Updated {item_type}s list: ")
+    print_items(input_list)
     with open(f"{item_type}.json", "w") as file:
-        json.dump(input_list, file)
+        json.dump(input_list, file, indent=4)
        
 
 def remove_item(input_list, item_type):
-    index = 1
-    for item in input_list:
-        print(f"[{index}] : {item} ")
-        index += 1
-    deleted_index = int(input(f"Which {item_type} do you want to delete? enter the code:\n" ))
-    input_list.pop(deleted_index - 1)
-    print(f"New {item_type}s list: {input_list}")
+    print_items(input_list)
+    chosen_number = get_correct_input(input_list, "remove")
+    input_list.pop(chosen_number)
+    temp_values = input_list.values()
+    input_list = {}
+    i = 1
+    for name in temp_values:
+        input_list[i] = name
+        i += 1
+    print(f"New {item_type}s list: ")
+    print_items(input_list)
     with open(f"{item_type}.json", "w") as file:
-        json.dump(input_list, file)
+        json.dump(input_list, file, indent=4)
 
 
 def add_order(input_list, c_list):
@@ -82,66 +86,83 @@ def add_order(input_list, c_list):
     new_order["customer_address"] = input("Please enter the address: \n")
     new_order["customer_phone"] = input("Please enter the phone number: \n")
     print("List of couriers: ")
-    index = 1
-    for item in c_list:
-        print(f"[{index}] : {item} ")
-        index += 1
+    print_items(c_list)
     new_order["courier"] = int(input("Which courier do you want to assign? enter the code:\n"))
     new_order["status"] = "preparing"
     print(f"New order: {new_order}")
-    input_list.append(new_order)
+    new_number = str(len(input_list) + 1)
+    input_list[new_number] = new_order
     with open("orders.json", "w") as file:
         json.dump(input_list, file, indent=4)
 
 
-def update_order(input_list):
-    keys = ["customer_name", "customer_address", "customer_phone", "courier", "status"]
+def change_order_status(input_list):
+    statuses = ["Preparing", "Sent", "Delivered"]
+    print_items(input_list)
+    order_number = get_correct_input(input_list, "update status")
+    status_index = 1
+    for status in statuses:
+        print(f"[{status_index}] : {status}")
+        status_index += 1
+    input_list[order_number]["status"] = statuses[int(input("Which status is the order in? Enter the code: ")) - 1]
+    print(f"New status: {input_list[order_number]}")
+    with open("orders.json", "w") as file:
+        json.dump(input_list, file, indent=4)
+
+
+def update_order(input_list, c_list):
+    elements = ["customer_name", "customer_address", "customer_phone", "courier", "status"]
     key_index = 1
-    order_index = 1
-    for order in input_list:
-        print(f"[{order_index}] : {order['customer_name']}'s order")
-        order_index += 1
-    updated_index = int(input("Which order do you want to update? Enter the code: "))
-    for key in input_list[updated_index - 1]:
-        if key == "status" or key == "courier":
+    print_items(input_list)
+    order_number = get_correct_input(input_list, "update")
+    for element in elements:
+        if element == "status":
             continue
-        print(f"[{key_index}] : {key}")
+        print(f"[{key_index}] : {element}")
         key_index += 1
-    which_key_to_update = keys[int(input("Which key do you want to update? Enter the code: ")) - 1]
-    new_value = input("Enter the new value: ")
+    which_element_to_update = elements[int(input("Which part do you want to update? Enter the code: ")) - 1]
+    if which_element_to_update == "courier":
+        print_items(c_list)
+        chosen_courier_index = str(input("Which courier do you want to chose? Enter the code: "))
+        new_value = c_list[chosen_courier_index]
+    else:
+        new_value = input("Enter the new value: ")
     if new_value != "":
-        input_list[updated_index -1][which_key_to_update] = new_value
+        input_list[order_number][which_element_to_update] = new_value
+    print(f"Updated order: {input_list[order_number]}")
     with open("orders.json", "w") as file:
         json.dump(input_list, file, indent=4)
     
             
-def change_order_status(input_list):
-    statuses = ["Preparing", "Sent", "Delivered"]
-    order_index = 1
-    status_index = 1
-    for order in input_list:
-        print(f"[{order_index}] : {order['customer_name']}'s order")
-        order_index += 1
-    changed_status_index = int(input("Which order do you want to update the status? Enter the code: "))
-    for status in statuses:
-        print(f"[{status_index}] : {status}")
-        status_index += 1
-    input_list[changed_status_index - 1]["status"] = statuses[int(input("Which status is the order in? Enter the code: ")) - 1]
-    with open("orders.json", "w") as file:
-        json.dump(input_list, file, indent=4)
-
-
 def remove_order(input_list):
     order_index = 1
-    for order in input_list:
-        print(f"[{order_index}] : {order['customer_name']}'s order")
-        order_index += 1
-    deleted_index = int(input("Which order do you want to delete? Enter the code: "))
-    input_list.pop(deleted_index - 1)
+    print_items(input_list)
+    deleted_index = (input("Which order do you want to delete? Enter the code: "))
+    input_list.pop(deleted_index)
+    temp_values = input_list.values()
+    input_list = {}
+    i = 1
+    for customer in temp_values:
+        input_list[i] = customer
+        i += 1
     with open("orders.json", "w") as file:
         json.dump(input_list, file, indent=4)
 
 
+def print_items(input_dict):
+    for key in input_dict:
+        print(f"[{key}] : {input_dict[key]}")
 
+
+def get_correct_input(input_list, command):
+    false_input = True
+    while false_input:
+        chosen_number = input(f"Which item do you want to {command}? enter the code: " )
+        acceptable_input = input_list.keys()
+        if chosen_number not in acceptable_input:
+            print("Enter the correct input please!")
+        else:
+            false_input = False
+    return chosen_number
 
 
--- main.py
+++ main.py
@@ -8,24 +8,29 @@ def main_menu():
     if first_choice == "0":
         exit()
     elif first_choice == "1":
+        print("*** Product Menu ***")
         second_choice = input(product_menu_message)
         while second_choice != "0":
             with open("product.json", "r") as product_file:
                 product_list = json.load(product_file)
             products_menu(second_choice, product_list)
+            print("\n*** Product Menu ***")
             second_choice = input(product_menu_message)
         if second_choice == "0":
             main_menu()
     elif first_choice == "2":
+        print("*** Courier Menu ***")
         second_choice = input(courier_menu_message)
         while second_choice != "0":
             with open("courier.json", "r") as courier_file:
                 courier_list = json.load(courier_file)
             couriers_menu(second_choice, courier_list)
+            print("*** Courier Menu ***")
             second_choice = input(courier_menu_message)
         if second_choice == "0":
             main_menu()
     elif first_choice == "3":
+        print("*** Orders Menu ***")
         second_choice = input(order_menu_message)
         while second_choice != "0":
             with open("courier.json", "r") as courier_file:
