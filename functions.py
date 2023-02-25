570fc792919463c23543975058250335d1d739b2
Saman <savedoroudi@gmail.com>
2023-02-25 19:34:27 +1100

    adding Database for products and couriers

--- functions.py
+++ functions.py
@@ -1,16 +1,40 @@
 import json
 from messages import *
 from tabulate import tabulate
+import pymysql.cursors
+import os
+from dotenv import load_dotenv
+load_dotenv(".env")
+HOST = os.environ.get("HOST")
+USER_NAME = os.environ.get("USER_NAME")
+PASSWORD = os.environ.get("PASSWORD")
+DATABASE = os.environ.get("DATABASE")
+
+connection = pymysql.connect(host=HOST,
+                             user=USER_NAME,
+                             password=PASSWORD,
+                             database=DATABASE,
+                             cursorclass=pymysql.cursors.DictCursor)
+
+
+def connet_to_db():
+    connection = pymysql.connect(host=HOST,
+                                user=USER_NAME,
+                                password=PASSWORD,
+                                database=DATABASE,
+                                cursorclass=pymysql.cursors.DictCursor)
+    return connection
+
 
 def products_menu(product_menu_choice, p_list):
     if product_menu_choice == "1":
         print_items(p_list, "Products")
     elif product_menu_choice == "2":
-        add_product(p_list)
+        add_product()
     elif product_menu_choice == "3":
         update_product(p_list)
     elif product_menu_choice == "4":
-        remove_item(p_list, "product")
+        remove_product(p_list, "Products")
     else:
         print(wrong_input)
 
@@ -19,11 +43,11 @@ def couriers_menu(courier_menu_choice, c_list):
     if courier_menu_choice == "1":
         print_items(c_list, "Couriers")
     elif courier_menu_choice == "2":
-        add_courier(c_list)
+        add_courier()
     elif courier_menu_choice == "3":
         update_courier(c_list)
     elif courier_menu_choice == "4":
-        remove_item(c_list, "courier")
+        remove_courier(c_list, "Couriers")
     else:
         print(wrong_input)
 
@@ -38,67 +62,70 @@ def orders_menu(order_menu_choice, order_list, c_list, p_list):
     elif order_menu_choice == "4":
         update_order(order_list, c_list, p_list)
     elif order_menu_choice == "5":
-        remove_item(order_list, "order")
+        remove_order(order_list, "order")
     else:
         print(wrong_input)
 
           
-def add_product(input_list : dict):
+def add_product():
     product_name = input("Enter new product's name: ")
     product_price = input("Enter the price: ")
-    new_number = str(len(input_list) + 1)
-    input_list[new_number] = {"Name" : product_name, "Price" : product_price}
+    cursor = connection.cursor()
+    sql = "INSERT INTO `products` (`product_name`, `product_price`) VALUES (%s, %s)"
+    cursor.execute(sql, (product_name, product_price))
+    connection.commit()
+
+
+def add_courier():
+    c_name = input("Enter new courier's name: ")
+    c_phone = input("Enter the phone number: ")
+    cursor = connection.cursor()
+    sql = "INSERT INTO `couriers` (`courier_name`, `courier_phone`) VALUES (%s, %s)"
+    cursor.execute(sql, (c_name, c_phone))
+    connection.commit()
+
+
+def update_product(input_list):
     print_items(input_list, "Products")
-    write_file("product.json", input_list)
+    chosen_id = input("Which product do you want to update? enter the ID: ")
+    updated_name = input("Enter the new product's name: ")
+    updated_price = input("Enter the new price: ")
+    cursor = connection.cursor()
+    sql = "UPDATE `products` SET `product_name` = %s, `product_price` = %s  WHERE `product_id` = %s"
+    cursor.execute(sql, (updated_name, updated_price, chosen_id))
+    connection.commit()
+    connet_to_db()
 
 
-def add_courier(input_list : dict):
-    courier_name =  input("Enter new courier's name: ")
-    courier_phone = input("Enter the phone number: ")
-    new_number = str(len(input_list) + 1)
-    input_list[new_number] = {"Name" : courier_name, "Phone" : courier_phone}
+
+def update_courier(input_list):
     print_items(input_list, "Couriers")
-    write_file("courier.json", input_list)
-
-
-def update_product(input_list : dict):
-    print_items(input_list, "Products")
-    chosen_number = get_correct_input(input_list, "update")
-    updated_name = input(f"Enter the new product's name. (If you don't want to change, just press ENTER): ")
-    if updated_name != "" :
-        input_list[chosen_number]["Name"] = updated_name
-    updated_price = input(f"Enter the new price. (If you don't want to change, just press ENTER): ")
-    if updated_price != "" :
-        input_list[chosen_number]["Price"] = updated_price
-    print_items(input_list, "Products")
-    write_file("product.json", input_list)
-
-
-def update_courier(input_list : dict):
-    print_items(input_list, "Couriers")
-    chosen_number = get_correct_input(input_list, "update")
-    updated_name = input(f"Enter the new courier's name. (If you don't want to change, just press ENTER): ")
-    if updated_name != "" :
-        input_list[chosen_number]["Name"] = updated_name
-    updated_phone = input(f"Enter the new phone number. (If you don't want to change, just press ENTER): ")
-    if updated_phone != "" :
-        input_list[chosen_number]["Phone"] = updated_phone
-    print_items(input_list, "Couriers")
-    write_file("courier.json", input_list)
+    chosen_id = input("Which courier do you want to update? enter the ID: ")
+    updated_name = (input(f"Enter the courier's name: "))
+    updated_phone = input(f"Enter the new phone number: ")
+    cursor = connection.cursor()
+    sql = "UPDATE `couriers` SET `courier_name` = %s, `courier_phone` = %s  WHERE `courier_id` = %s"
+    cursor.execute(sql, (updated_name, updated_phone, chosen_id))
+    connection.commit()
    
        
-def remove_item(input_list : dict, input_name : str):
-    print_items(input_list, f"{input_name}s" )
-    chosen_number = get_correct_input(input_list, "remove")
-    input_list.pop(chosen_number)
-    temp_values = input_list.values()
-    input_list = {}
-    i = 1
-    for name in temp_values:
-        input_list[i] = name
-        i += 1
-    print_items(input_list, f"{input_name}s" )
-    write_file(f"{input_name}.json", input_list)
+def remove_product(input_list : dict, input_name : str):
+    print_items(input_list, input_name)
+    chosen_id = int(input("Which product do you want to delete? enter the ID: "))
+    cursor = connection.cursor()
+    sql = "DELETE FROM `products` WHERE `product_id` = %s"
+    cursor.execute(sql, chosen_id)
+    connection.commit()
+
+
+def remove_courier(input_list, input_name : str):
+    print_items(input_list, input_name)
+    chosen_id = int(input("Which courier do you want to delete? enter the ID: "))
+    # connet_to_db()
+    cursor = connection.cursor()
+    sql = "DELETE FROM `couriers` WHERE `courier_id` = %s"
+    cursor.execute(sql, chosen_id)
+    connection.commit()
 
 
 def add_order(input_list, c_list, p_list):
@@ -173,11 +200,25 @@ def update_order(input_list, c_list, p_list):
         json.dump(input_list, file, indent=4)
     
 
-def print_items(input_dict : dict, input_name : str):
-    print(f"\n## {input_name} ##")
-    for key in input_dict:
-        print(f"[{key}] : {input_dict[key]}")
+def remove_order(input_list : dict, input_name : str):
+    print_items(input_list, input_name)
+    chosen_id = input("Which order do you want to delete? enter the ID: ")
+    input_list.pop(chosen_id)
+    temp_values = input_list.values()
+    input_list = {}
+    i = 1
+    for content in temp_values:
+        input_list[i] = content
+        i += 1
+    print_items(input_list, f"{input_name}s" )
+    write_file(f"{input_name}.json", input_list)
 
+
+def print_items(input_dict, input_name : str):
+    print(f"\n## {input_name} ##")
+    for i in input_dict:
+        print(i)
+        
 
 def print_orders(order_list):
     headers = ["Order No.", "Customer Name", "Customer Address", "Customer Phone", "Items", "Courier", "Status" ]
@@ -188,17 +229,31 @@ def print_orders(order_list):
     print(tabulate(rows, headers=headers, numalign = "center" ))
 
 
-def get_correct_input(input_list, command):
-    false_input = True
-    while false_input:
-        chosen_number = input(f"Which item do you want to {command}? enter the code: " )
-        acceptable_input = input_list.keys()
-        if chosen_number not in acceptable_input:
-            print("*** Enter the correct input please! ***")
-        else:
-            false_input = False
-    return chosen_number
+# def get_correct_input(input_list, command):
+#     false_input = True
+#     while false_input:
+#         user_input = input(f"Which item do you want to {command}? enter the code: " )
+#         acceptable_input = input_list.keys()
+#         if user_input not in acceptable_input:
+#             print("*** Enter the correct input please! ***")
+#         else:
+#             false_input = False
+#     return user_input
 
+def get_correct_input(input_list : dict, command):
+    valid_inputs = []
+    i = 1
+    for key in input_list.keys():
+        valid_inputs.append(key)
+    while True: 
+        try:
+            user_input = input(f"Which item do you want to {command}? Enter the code: ")
+            if user_input not in valid_inputs:
+                raise ValueError
+            break
+        except ValueError:
+            print(" *** Invalid input. Please enter one of the above codes. *** ")
+    return user_input
 
 def read_file(dict_name : str):
     with open(dict_name) as file:
--- main.py
+++ main.py
@@ -1,9 +1,34 @@
 import json
 from messages import *
 from functions import *
+import pymysql.cursors
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv(".env")
+
+
+HOST = os.environ.get("HOST")
+USER_NAME = os.environ.get("USER_NAME")
+PASSWORD = os.environ.get("PASSWORD")
+DATABASE = os.environ.get("DATABASE")
+
+
+def connet_to_db():
+    connection = pymysql.connect(host=HOST,
+                                user=USER_NAME,
+                                password=PASSWORD,
+                                database=DATABASE,
+                                cursorclass=pymysql.cursors.DictCursor)
+
+    return connection
+
+
 
 def main_menu():
     print("WELCOME to Main Menu!")
+   
     first_choice = input(main_menu_message)
     if first_choice == "0":
         exit()
@@ -11,7 +36,12 @@ def main_menu():
         print("*** Product Menu ***")
         second_choice = input(product_menu_message)
         while second_choice != "0":
-            product_list = read_file("product.json")
+            connet_to_db()
+            cursor = connection.cursor()
+            sql = "SELECT `product_id`, `product_name`, `product_price` FROM `products`"
+            cursor.execute(sql)
+            product_list = cursor.fetchall()
+            # product_list = read_file("product.json")
             products_menu(second_choice, product_list)
             print("\n*** Product Menu ***")
             second_choice = input(product_menu_message)
@@ -21,7 +51,11 @@ def main_menu():
         print("*** Courier Menu ***")
         second_choice = input(courier_menu_message)
         while second_choice != "0":
-            courier_list = read_file("courier.json")
+            connet_to_db()
+            cursor = connection.cursor()
+            sql = "SELECT `courier_id`, `courier_name`, `courier_phone` FROM `couriers`"
+            cursor.execute(sql)
+            courier_list = cursor.fetchall()
             couriers_menu(second_choice, courier_list)
             print("\n*** Courier Menu ***")
             second_choice = input(courier_menu_message)
